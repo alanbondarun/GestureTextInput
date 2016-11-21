ba559644d4f360ce94387a6353a7fe86d9f43e1f
alanb <efik722life@gmail.com>
2016-11-22 02:41:53 +0900

    add base activities for glass development

---
+++ glass/src/main/java/com/alanb/gesturetextinput/GlassFrontActivity.java
@@ -0,0 +1,86 @@
+package com.alanb.gesturetextinput;
+
+import com.google.android.glass.widget.CardBuilder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+public class GlassFrontActivity extends Activity
+{
+    private final String TAG = this.getClass().getSimpleName();
+    private View mView;
+
+    @Override
+    protected void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+
+        mView = buildView();
+        setContentView(mView);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.front_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Intent intent = null;
+        switch (item.getItemId())
+        {
+            case R.id.oned_menu_item:
+                intent = new Intent(this, GlassOneDActivity.class);
+                break;
+            case R.id.watch_menu_item:
+                intent = new Intent(this, GlassWatchWriteActivity.class);
+                break;
+        }
+        if (intent != null)
+        {
+            startActivity(intent);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER)
+        {
+            openOptionsMenu();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private View buildView()
+    {
+        CardBuilder card = new CardBuilder(this, CardBuilder.Layout.EMBED_INSIDE);
+        card.setEmbeddedLayout(R.layout.glass_front_layout);
+        return card.getView();
+    }
+}
---
+++ glass/src/main/java/com/alanb/gesturetextinput/GlassWatchWriteActivity.java
@@ -0,0 +1,28 @@
+package com.alanb.gesturetextinput;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.glass.widget.CardBuilder;
+
+public class GlassWatchWriteActivity extends Activity
+{
+    private View mView;
+
+    @Override
+    protected void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+
+        mView = buildView();
+        setContentView(mView);
+    }
+
+    private View buildView()
+    {
+        CardBuilder card = new CardBuilder(this, CardBuilder.Layout.EMBED_INSIDE);
+        card.setEmbeddedLayout(R.layout.glass_watch_layout);
+        return card.getView();
+    }
+}
